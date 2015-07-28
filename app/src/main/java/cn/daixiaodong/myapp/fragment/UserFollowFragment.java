b07ef6dd4cf459d0c54dbe27b1ebe93cd5cfb65f
CodingAdai <496690324@qq.com>
2015-07-28 17:34:59 +0800

    完善用户未登录时的界面逻辑

--- app/src/main/java/cn/daixiaodong/myapp/fragment/UserFollowFragment.java
+++ app/src/main/java/cn/daixiaodong/myapp/fragment/UserFollowFragment.java
@@ -1,6 +1,7 @@
 package cn.daixiaodong.myapp.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
@@ -22,11 +24,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cn.daixiaodong.myapp.R;
+import cn.daixiaodong.myapp.activity.SignInActivity;
+import cn.daixiaodong.myapp.activity.SignInActivity_;
 import cn.daixiaodong.myapp.activity.UserProfileActivity_;
 import cn.daixiaodong.myapp.adapter.UserFollowListAdapter;
 import cn.daixiaodong.myapp.fragment.common.BaseFragment;
-
-import static android.support.v7.widget.RecyclerView.OnScrollListener;
 
 
 /**
@@ -34,18 +36,21 @@ import static android.support.v7.widget.RecyclerView.OnScrollListener;
  */
 public class UserFollowFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener, UserFollowListAdapter.OnItemClickListener {
 
-    private View mRootView;
+    private View mConvertView;
     private SwipeRefreshLayout mRefreshLayout;
     private RecyclerView mRecyclerView;
     private UserFollowListAdapter mAdapter;
     private List<AVObject> mData;
+    // 登录提示 View
+    private View mSignInTip;
 
+    private int mOffset;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mRootView
+        mConvertView
                 = inflater.inflate(R.layout.fragment_follow, container, false);
-        return mRootView;
+        return mConvertView;
     }
 
     @Override
@@ -55,17 +60,21 @@ public class UserFollowFragment extends BaseFragment implements SwipeRefreshLayo
     }
 
     private void initViews() {
-        mRefreshLayout = (SwipeRefreshLayout) mRootView.findViewById(R.id.id_srl_refresh_data);
-        mRecyclerView = (RecyclerView) mRootView.findViewById(R.id.id_rv_collect_recycler_view);
+        mRefreshLayout = (SwipeRefreshLayout) mConvertView.findViewById(R.id.id_srl_refresh_data);
+        mRecyclerView = (RecyclerView) mConvertView.findViewById(R.id.id_rv_collect_recycler_view);
         setUpRecyclerView();
         setRefreshLayout();
-        mRefreshLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                mRefreshLayout.setRefreshing(true);
-                loadData(true);
-            }
-        });
+        if (isSignIn()) {
+            mRefreshLayout.post(new Runnable() {
+                @Override
+                public void run() {
+                    mRefreshLayout.setRefreshing(true);
+                    loadData(true);
+                }
+            });
+        } else {
+            updateUI();
+        }
     }
 
     private void setRefreshLayout() {
@@ -80,17 +89,7 @@ public class UserFollowFragment extends BaseFragment implements SwipeRefreshLayo
         mAdapter = new UserFollowListAdapter(getActivity(), mData);
         mAdapter.setOnItemClickListener(this);
         mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.addOnScrollListener(new OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-            }
 
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-            }
-        });
     }
 
     @Override
@@ -111,6 +110,7 @@ public class UserFollowFragment extends BaseFragment implements SwipeRefreshLayo
                         if (list.isEmpty()) {
 
                         } else {
+                            mData.clear();
                             mData.addAll(list);
                             mAdapter.notifyDataSetChanged();
                         }
@@ -122,6 +122,7 @@ public class UserFollowFragment extends BaseFragment implements SwipeRefreshLayo
         }
     }
 
+
     @Override
     public void onItemClick(UserFollowListAdapter.MyViewHolder viewHolder, int pos) {
         UserProfileActivity_.intent(this).start();
@@ -129,6 +130,72 @@ public class UserFollowFragment extends BaseFragment implements SwipeRefreshLayo
 
     @Override
     public void onUnfollowBtnClick(UserFollowListAdapter.MyViewHolder viewHolder, int pos) {
-        Log.i("按钮被点击了","第"+pos+"个");
+        Log.i("按钮被点击了", "第" + pos + "个");
+    }
+
+
+    /**
+     * 接收 SignInActivity 的返回数据
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SignInActivity.SIGN_IN_REQUEST_CODE) {
+            if (resultCode == SignInActivity.SIGN_IN_SUCCESS_RESULT_CODE) {
+                updateUI();
+                mRefreshLayout.setRefreshing(true);
+                loadData(true);
+            }
+        }
+
+    }
+
+
+
+    /**
+     * 当视图显示时，更新界面状态
+     *
+     * @param hidden
+     */
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden) {
+            updateUI();
+        }
+    }
+
+
+    /**
+     * 根据登录状态显示或隐藏  登录提示
+     */
+    public void updateUI() {
+        if (isSignIn()) {
+            if (mSignInTip != null) {
+                mSignInTip.setVisibility(View.GONE);
+                mRefreshLayout.setVisibility(View.VISIBLE);
+                mRecyclerView.setVisibility(View.VISIBLE);
+            }
+        } else {
+            if (mSignInTip == null) {
+                ViewStub viewStub = (ViewStub) mConvertView.findViewById(R.id.id_vs_login_in_prompt);
+                mSignInTip = viewStub.inflate();
+                mSignInTip.findViewById(R.id.id_btn_log_in).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 跳转到登录界面
+                        SignInActivity_.intent(UserFollowFragment.this).startForResult(SignInActivity.SIGN_IN_REQUEST_CODE);
+                    }
+
+                });
+            }
+            mRecyclerView.setVisibility(View.GONE);
+            mRefreshLayout.setVisibility(View.GONE);
+            mSignInTip.setVisibility(View.VISIBLE);
+        }
     }
 }
